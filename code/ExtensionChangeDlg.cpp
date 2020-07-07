514136fae2f2d7c78510630b7bdb1455eab7160d
matsuo <matsuo.tomoki@pilot-inc.co.jp>
2020-07-07 16:18:37 +0900

    変更

--- code/ExtensionChangeDlg.cpp
+++ code/ExtensionChangeDlg.cpp
@@ -364,7 +364,7 @@ BOOL CExtensionChangeDlg::GetFileList(CString path)
 			string str_filePath = CStringA(filePath).GetBuffer();
 			string str_PreviousExtension = CStringA(PreviousExtension).GetBuffer();
 
-			if (str_filePath.find(str_PreviousExtension)) {
+			if (str_filePath.find(str_PreviousExtension) && 0) {
 
 				m_list_displaypath.AddString(filePath);
 			}
